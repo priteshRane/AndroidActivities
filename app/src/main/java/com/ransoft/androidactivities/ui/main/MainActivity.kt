031b1c649ed9e53c9775ab86a8c72960127b247e
Pritesh_PC <priteshrane01@gmail.com>
2020-06-11 16:14:35 +0530

    Intent added to go second activity

--- app/src/main/java/com/ransoft/androidactivities/ui/main/MainActivity.kt
+++ app/src/main/java/com/ransoft/androidactivities/ui/main/MainActivity.kt
@@ -1,11 +1,13 @@
 package com.ransoft.androidactivities.ui.main
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.databinding.DataBindingUtil
 import androidx.lifecycle.ViewModelProvider
 import com.ransoft.androidactivities.R
 import com.ransoft.androidactivities.databinding.ActivityMainBinding
+import com.ransoft.androidactivities.ui.second.SecondActivity
 import com.ransoft.androidactivities.util.toast
 
 class   MainActivity : AppCompatActivity(), MainListener {
@@ -21,5 +23,7 @@ class   MainActivity : AppCompatActivity(), MainListener {
 
     override fun onSecondActivityClick() {
         toast("Go to second activity")
+        val intent = Intent(this, SecondActivity::class.java)
+        startActivity(intent)
     }
 }
